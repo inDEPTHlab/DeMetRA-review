3b5c7ae83e8eeb54bd6b9d6510b28bb7ab247b1d
SereDef <sere.def@gmail.com>
2024-11-13 16:47:49 +0100

    added plots, info card to fix

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -1,18 +1,30 @@
 # from shiny import App, render, ui
 from shiny import App, Inputs, Outputs, Session, module, reactive, render, ui
+import faicons as fa
 
 from shinywidgets import output_widget, render_plotly
 
 import definitions.layout_styles as styles
-from definitions.backend_funcs import _sample_size_over_time
+from definitions.backend_funcs import _count_papers, _multilevel_piechart, _sample_size_over_time, _category_over_years
 
-app_ui = ui.page_fillable(
+app_ui = ui.page_fluid(
     ui.card(
         ui.card_header("DeMetRA: Developmental Methylation Risk Atlas - literature review"),
         ui.layout_sidebar(
-            ui.sidebar("Sidebar",
-                       bg="#f8f8f8"),
-            output_widget('sample_size_over_time') ,
+            ui.sidebar("Sidebar", bg="#f8f8f8"),
+
+            ui.layout_column_wrap(
+                ui.value_box("Papers",
+                    ui.output_text('paper_count'), showcase=fa.icon_svg('file-circle-check')),
+
+                    # ui.value_box(
+                    #     "unique MPS",
+                    #     ui.output_text("bill_length"),
+                    #     showcase=fa.icon_svg("ruler-horizontal")),
+                fill=False),
+            ui.card(output_widget('multilevel_piechart')),
+            ui.card(output_widget('sample_size_over_time')),
+            ui.card(output_widget('category_over_years')),
         ),
     )
 )
@@ -21,9 +33,24 @@ app_ui = ui.page_fillable(
 def server(input, output, session):
 
     @output
+    @render.ui
+    def paper_count():
+        c = _count_papers()
+        return f'{c} papers'
+
+    @render_plotly
+    def multilevel_piechart():
+        p = _multilevel_piechart()
+        return p
+
     @render_plotly
     def sample_size_over_time():
         p = _sample_size_over_time()
+        return p
+
+    @render_plotly
+    def category_over_years():
+        p = _category_over_years()
         return p
 
 
--- src_shiny_app/definitions/backend_funcs.py
+++ src_shiny_app/definitions/backend_funcs.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from dateutil.parser import parse
 
 import plotly.express as px
 import textwrap
@@ -7,99 +6,28 @@ import textwrap
 import definitions.layout_styles as styles
 
 
-# ===== DATA PROCESSING FUNCTIONS ==============================================================
-
 results_directory = './assets/'
+data = pd.read_csv(f'{results_directory}clean_bib_metadata.csv', parse_dates=['Date'])
 
-d_raw = pd.read_excel(f'{results_directory}Dev_MPS_papers.xlsx', sheet_name='Read')
-# Only include "included" studies
-d = d_raw.loc[d_raw.Include == 'Yes']
-
-
-def is_date(string, fuzzy=False):
-    """
-    Return whether the string can be interpreted as a date.
-        :param string: str, string to check for date
-        :param fuzzy: bool, ignore unknown tokens in string if True
-    """
-    try:
-        parse(string, fuzzy=fuzzy)
-        return True
-    except ValueError:
-        return False
+# ================
 
 
-def extract_info(prefixes, line, index):
-    """ Extract information from bibliography """
-    # Date is not always correctly labelled
-    if is_date(line):
-        bib.loc[index, "Date"] = line[:-1]
-        return True
-
-    for prefix in prefixes:
-        if line.startswith(prefix):
-            bib.loc[index, prefix[:-2]] = line[len(prefix):-1]
-            break
+def _count_papers(d=data):
+    n = int(len(pd.unique(d['Identifier'])))
+    return n
 
 
-# Initialise
-n = 1
-bib = pd.DataFrame()
+def _count_mpss(d=data):
+    return d.shape[0]
 
-# Extract info
-with open(f'{results_directory}Step2_AfterAbstractScreening_2024-07-13.txt', 'r') as file:
-    for line in file:
-        if line.startswith('Reference Type'):
-            continue
-        if line.startswith('Record Number'):
-            # record counter updated for every new record
-            n = int(''.join(i for i in line if i.isdigit()))
-            bib.loc[n-1, 'Identifier'] = int(n)
 
-        extract_info([s + ': ' for s in ['Author', 'Year', 'Title', 'Journal', 'Abstract', 'Date',
-                                         'Short Title', 'DOI', 'URL']], line, n-1)
-        # Other fields
-        # 'Volume', 'Issue', 'Pages', 'ISSN', 'Accession Number', 'Label', 'Type of Article',
-        # 'Notes', 'Author Address', 'Name of Database', 'Language'
-
-# TODO: extract keywords
-# bib.set_index('Identifier');
-
-# Correct dates
-bib['Date2'] = pd.Series([' '.join([d, y]) if y not in d else d if not d == 'nan' else y for d, y in zip(
-    bib.Date.map(str), bib.Year.map(str))])
-bib['Date2'] = bib.Date2.apply(lambda date: parse(date).strftime('%Y-%m-%d') if 'nan' not in date else parse(
-    '01 01' + date[3:]).strftime('%Y-%m-%d'))
-
-# Check
-# pd.options.display.max_columns = 200
-# bib[['Date', 'Year', 'Date2']]
-
-# NOTES: some dates are not correct (years do not match)
-#        if only year is available set date to 01/01
-#        if only month is available set day to 14th
-
-data = pd.merge(d, bib[['Identifier', 'Date2', 'Short Title', 'Abstract', 'URL']],
-                how='left', on='Identifier')
-
-# Some other tmp clean-ups:
-data.loc[data.Phenotype.str.contains('BAFopathy syndrome: Coffin-Siris'), 'Category'] = 'Syndrome'
-data.loc[data.Phenotype.str.contains('Intellectual developmental disorder, X-linked'), 'Category'] = 'Psychiatric'
-
-# Sample size to numeric
-data['Sample size'] = pd.to_numeric(data['Sample size'], errors='coerce')  # removed the "- check"
-# Dates to dates
-data['Date'] = pd.to_datetime(data['Date2'], errors='coerce')
-
-# Combine array info
-data['Array'] = [i if i != 'Multiple' else j for i, j in zip(data['Array'], data['Multiple_array'])]
-
-# NOTE: DOI info could be correct in bib in some cases...?
-
-# ================ ##############################
+# ================ PLOTTING ================
 
 
 def _sample_size_over_time(data=data, color_by="Category", log_sample_size=True, model_type="ols", scope="overall"):
+    """
+    Scatterplot of sample size (y axis) vs. publication date (x axis)
+    """
     # Color set-up
     if color_by in styles.COLOR_MAPS.keys():
         color_map = styles.COLOR_MAPS[color_by]
@@ -124,5 +52,43 @@ def _sample_size_over_time(data=data, color_by="Category", log_sample_size=True,
     fig.update_yaxes(title_text=f"<b>Sample size</b>{ylabel_note}", **axes_style)
     fig.update_xaxes(title_text="Publication date", **axes_style)
     fig.update_layout(plot_bgcolor="whitesmoke", width=1300, height=400, margin=dict(l=10, r=10, t=25, b=10))
+
+    return fig
+
+
+def _category_over_years(data=data, color_by="Category", percent=True, width=1300, height=400):
+    """
+       Histogram of counts (y axis) vs. publication year (x axis)
+    """
+    # Color set-up
+    if color_by in styles.COLOR_MAPS.keys():
+        color_map = styles.COLOR_MAPS[color_by]
+    else:
+        color_map = "Virdis"
+
+    norm = "percent" if percent else None
+
+    fig = px.histogram(data, x="Year", color=color_by, barnorm=norm,
+                       color_discrete_map=color_map,
+                       title=f"{color_by} over time", width=width, height=height)
+
+    return fig
+
+def _multilevel_piechart(lvl1='Category', lvl2='Phenotype'):
+
+    counts = data[[lvl1, lvl2]].value_counts().reset_index()
+
+    # Color set-up
+    color_by = lvl1
+    if color_by in styles.COLOR_MAPS.keys():
+        color_map = styles.COLOR_MAPS[color_by]
+    else:
+        color_map = "Virdis"
+
+    fig = px.sunburst(counts,
+                      path=[lvl1, lvl2], values='count',
+                      color = color_by, color_discrete_map = color_map,
+                      width=1200, height=750,
+                      title="Number of MPSs per phenotype")
 
     return fig
